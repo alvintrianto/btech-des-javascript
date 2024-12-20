3f5b6a18e81a77d6ee2c69e463c225a93764e4e3
alvintrianto <alvin.trianto97@gmail.com>
2024-12-20 13:56:49 +0700

    First commit

---
+++ intro.js
@@ -0,0 +1,21 @@
+console.log("Hello World")
+console.log("Hello World 2")
+console.log("Hello World 3")
+
+let title = "Judul"
+const title2 = "Bersama"
+
+console.log(title)
+
+
+let discountPrice = 2500
+let discountPrice2 = "2500"
+
+function assertTitle(){
+    //a
+    //b
+    //c
+}
+
+assertTitle()
+assertTitle()
